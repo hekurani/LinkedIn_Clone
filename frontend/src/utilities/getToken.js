fab51444e4142c266c936dcb6a9e8324ec216431
Ferat03 <fg57925@ubt-uni.net>
2024-05-04 18:10:41 +0200

    Token only for auth for AuthUser decorator

--- frontend/src/utilities/getToken.js
+++ frontend/src/utilities/getToken.js
@@ -1,18 +1,22 @@
 import { jwtDecode } from "jwt-decode";
 const getToken = () => {
-    const token = localStorage.getItem("access_token");
-    if (!token) return null;
-    const decoded = jwtDecode(token);
-    if (decoded) return decoded;
-    return null;
-  };
-  
-  const getRefreshToken = () => {
-    const refreshToken = localStorage.getItem("refresh_token");
-    if (!refreshToken) return null;
-    const decoded = jwtDecode(refreshToken);
-    if (decoded) return decoded;
-    return null;
-  };
-  
-  export { getToken, getRefreshToken };
+  const token = localStorage.getItem("access_token");
+  if (!token) return null;
+  const decoded = jwtDecode(token);
+  if (decoded) return decoded;
+  return null;
+};
+const getTokenForAxios = () => {
+  const token = localStorage.getItem("access_token");
+  if (!token) return null;
+  return token;
+};
+const getRefreshToken = () => {
+  const refreshToken = localStorage.getItem("refresh_token");
+  if (!refreshToken) return null;
+  const decoded = jwtDecode(refreshToken);
+  if (decoded) return decoded;
+  return null;
+};
+
+export { getToken, getRefreshToken, getTokenForAxios };
