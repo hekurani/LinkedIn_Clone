7300f500033f8448f335ef8d868a0e2db9809a56
Ferat03 <fg57925@ubt-uni.net>
2024-05-27 16:47:17 +0200

    invitation component responsive!

--- frontend/src/Components/Connection/Invitation.jsx
+++ frontend/src/Components/Connection/Invitation.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { acceptRequest, cancelRequest, getRequestSendedToMe } from "../../utilities/friends/getFriends";
 import defaultImage from "../../assets/default.png";
 import { Link } from "react-router-dom";
+
 const Invitation = () => {
   const [data, setData] = useState([]);
 
@@ -14,52 +15,45 @@ const Invitation = () => {
   }, []);
 
   const cancelFriendRequest = async (id) => {
-      await cancelRequest(id);
-      const data = await getRequestSendedToMe();
-      setData(data); //to improve
+    await cancelRequest(id);
+    const updatedData = await getRequestSendedToMe();
+    setData(updatedData);
   }
+
   const acceptFriendRequest = async (id) => {
     await acceptRequest(id);
-    const data = await getRequestSendedToMe();
-    setData(data); //to improve
-}
+    const updatedData = await getRequestSendedToMe();
+    setData(updatedData);
+  }
 
   return (
-    <div
-      style={{ border: "1px solid #d3d3d3 ", width: "880px",borderRadius:'7px' }}
-      className="mx-auto p-5 mt-3"
-    >
+    <div style={{ border: "1px solid #d3d3d3 ", maxWidth: "880px", borderRadius: '7px' }} className="mx-auto p-5 mt-3">
       <div className="header flex items-center mb-3">
         <p className="mr-auto m-1">Invitations</p>
         <p className="m-3 font-semibold">{!!data && data.length > 0 ? `See all ${data.length}` : ''}</p>
       </div>
-      <div className="content flex">
-      {data.map((request) => (
-          <>
-          <Link to={`/${request.sender.id}/profile`}>
-         
-            <div key={request.id} className="invitation-item grid grid-cols-3 ml-2">
+      <div className="content">
+        {data.map((request) => (
+          <div key={request.id} className="invitation-item flex items-center justify-between mb-4 p-2 border-b">
+            <Link to={`/${request.sender.id}/profile`} className="flex items-center">
               <img
                 src={request.sender.imageProfile || defaultImage}
                 alt={`${request.sender.name}`}
-                className="w-12 h-12 rounded-full"
+                className="w-12 h-12 rounded-full mr-3"
               />
-              <p className="font-semibold mt-2">
-                {request.sender.name} {request.sender.lastname}
-              </p>
-            </div>
+              <p className="font-semibold">{request.sender.name} {request.sender.lastname}</p>
             </Link>
-            <div className="flex  ml-auto mr-3">
-              <button onClick={(() => cancelFriendRequest(request.id))} className="mr-3">Ignore</button>
+            <div className="flex items-center">
+              <button onClick={() => cancelFriendRequest(request.id)} className="mr-3">Ignore</button>
               <button
-              onClick={(() => acceptFriendRequest(request.id))}
-                style={{ color:'#0a66c2',border:'1px solid #0a66c2',borderRadius:'10px' }}
-                className="h-12 w-20 font-semibold"
+                onClick={() => acceptFriendRequest(request.id)}
+                style={{ color: '#0a66c2', border: '1px solid #0a66c2', borderRadius: '10px' }}
+                className="h-10 w-20 font-semibold"
               >
                 Accept
-                </button>
+              </button>
             </div>
-          </>
+          </div>
         ))}
       </div>
     </div>
